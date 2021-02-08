65de676f09f273162cd191e563783ba1ec70908e
matinsayed <matin_sayed@yahoo.com>
2021-02-08 15:10:18 +0530

    Update PostInstallationScript.ps1

--- PostInstallationScript.ps1
+++ PostInstallationScript.ps1
@@ -1,5 +1,5 @@
 Install-WindowsFeature Web-WebServer, Web-Mgmt-Tools, Web-Mgmt-Console, Web-Basic-Auth, Web-Net-Ext, Web-Net-Ext45, Web-Asp-Net, Web-Asp-Net45, NET-Framework-Core, NET-HTTP-Activation, NET-Framework-45-ASPNET, NET-WCF-HTTP-Activation45, PowerShell, PowerShell-V2, WAS-Process-Model, WAS-NET-Environment, WAS-Config-APIs
-New-NetFirewallRule -DisplayName 'Application_Port' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 5005
+New-NetFirewallRule -DisplayName 'Application_Port' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 5001, 5002, 5005
 
 $url = "https://github.com/deploymentfiles/powershellscript/raw/master/sqlncli.msi"
 mkdir c:\sqlnative
